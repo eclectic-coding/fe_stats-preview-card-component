312459ee0f551c41c1ea057da1ebae947640fab7
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-06-15 14:05:02 -0400

    Set up gulp and sass to deploy

---
+++ gulp.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // Settings: Turn on/off build features
+  clean: true,
+  styles: true,
+  images: true,
+  scripts: false,
+  copy: false,
+  reload: true,
+
+  // Global project folder destinations
+  input: "src/",
+  html: "src/",
+  output: "dist/",
+
+  // Styles
+  stylesSrc: "src/scss/**/*.{scss,sass}",
+  stylesDest: "dist/",
+
+  // Images
+  imagesSrc: "src/images/*.*",
+  imagesDest: "dist/images",
+
+  // Scripts
+  scriptsSrc: "src/js/*.js",
+  scriptsDest: "dist/",
+
+  // reload
+  reload: "dist/",
+};
